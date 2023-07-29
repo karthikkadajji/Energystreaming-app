148f4951071279f962a8c56ac6dd0ce9780c5b1e
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-29 21:01:29 +0200

    test with schema creation

--- eventhubsend/__init__.py
+++ eventhubsend/__init__.py
@@ -14,29 +14,22 @@ GROUP_NAME = "datacapture"
 def get_or_register_schema(schema_registry_client):
     # Define your Avro schema here
     schema_definition = json.dumps(SCHEMA_JSON, separators=(",", ":"))
-    schema_properties = schema_registry_client.register_schema("datacapture", "spotmarktpreis", schema_definition,
+    schema_properties = schema_registry_client.register_schema("datacapture", "spotmarktpreis1", schema_definition,
                                                                "Avro")
     return schema_properties
 
 
 def send_data_to_eventhub():
-    data = {
-        "Datum": "31.12.2020",
-        "von": "23:00",
-        "Zeitzone_von": "UTC",
-        "bis": "00:00",
-        "Zeitzone_bis": "UTC",
-        "Spotmarktpreis_in_ct_kWh": "5,087",
-    }
+    data = {"name": "Ben", "favorite_number": 7, "favorite_color": "red"}
+
     avro_encoder = AvroEncoder(
-        SchemaRegistrySingleton.get_instance(),
+        client=SchemaRegistrySingleton.get_instance(),
         group_name=GROUP_NAME,
-        auto_register=True
     )
 
-    avro_encoded_data = avro_encoder.encode(data, message_type=EventData)
+    avro_encoded_data = avro_encoder.encode(data,schema=SCHEMA_JSON, message_type=EventData)
 
-    producer = EventHubProducerClient(EVENTHUB_NAME, credential=DefaultAzureCredential())
+    producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://energy-application-eh-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rhVUIDr1TgGofv7wdXgs45f1I3jJ1HlX++AEhDDEwfs=",eventhub_name=EVENTHUB_NAME,credential=DefaultAzureCredential)
     with producer:
         producer.send_batch(avro_encoded_data)
     # for line in data.strip().split("\n"):
@@ -46,11 +39,13 @@ def send_data_to_eventhub():
     #         producer.send(event_data)
 
 
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # create singleton client connection
     client = SchemaRegistrySingleton.get_instance()
     schema_properties = get_or_register_schema(client)
-    send_data_to_eventhub()
+    #send_data_to_eventhub()
     schema_properties.id
     send_data_to_eventhub()
-    return func.HttpResponse(f"Schema properties are {schema_properties}")
+    return func.HttpResponse(f"Schema properties are")
