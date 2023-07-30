6d8729300361d39eadca784e8b2f34a3bbcec603
karthikkadajji <karthik.kadajji@gmail.com>
2023-07-30 08:26:25 +0200

    test with schema creation

--- shared/schema_format/Spotmarktpreis_schema.py
+++ shared/schema_format/Spotmarktpreis_schema.py
@@ -1,11 +1,11 @@
 # Define the Avro schema in a separate module
-SCHEMA_JSON = 
-{"namespace": "example.avro",
- "type": "record",
- "name": "User",
- "fields": [
-     {"name": "name", "type": "string"},
-     {"name": "favorite_number",  "type": ["int", "null"]},
-     {"name": "favorite_color", "type": ["string", "null"]}
+SCHEMA_JSON = {
+    "namespace": "example.avro",
+    "type": "record",
+    "name": "User",
+    "fields": [
+        {"name": "name", "type": "string"},
+        {"name": "favorite_number",  "type": ["int", "null"]},
+        {"name": "favorite_color", "type": ["string", "null"]}
  ]
 }
